d762d94b4e21bcb1454a26ad25fe8ea121cfb349
Dmitry Simonenko <pmwkaa@gmail.com>
2016-11-09 15:33:52 +0300

    sonata: add macro file

---
+++ lib/so_macro.h
@@ -0,0 +1,18 @@
+#ifndef SO_MACRO_H_
+#define SO_MACRO_H_
+
+/*
+ * sonata.
+ *
+ * Protocol-level PostgreSQL client library.
+*/
+
+#define so_likely(e)   __builtin_expect(!! (e), 1)
+#define so_unlikely(e) __builtin_expect(!! (e), 0)
+
+#define so_packed      __attribute__((packed))
+
+#define so_container_of(ptr, t, f) \
+	((t*)((char*)(ptr) - __builtin_offsetof(t, f)))
+
+#endif
