d8b0f84824a3969eafbec53e784b13598bbd408c
Dmitry Simonenko <pmwkaa@gmail.com>
2016-11-09 15:36:56 +0300

    sonata: add packet header definition

---
+++ lib/so_header.h
@@ -0,0 +1,18 @@
+#ifndef SO_HEADER_H_
+#define SO_HEADER_H_
+
+/*
+ * sonata.
+ *
+ * Protocol-level PostgreSQL client library.
+*/
+
+typedef struct soheader_t soheader_t;
+
+struct soheader_t {
+	uint8_t  type;
+	uint32_t len;
+	uint8_t  data[];
+} so_packed;
+
+#endif
