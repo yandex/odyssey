46ac6dc4b20b716f52e4c7e55892a1dd91039cc4
Dmitry Simonenko <pmwkaa@gmail.com>
2016-11-09 16:08:10 +0300

    sonata: startup packet reader

--- lib/so_read.c
+++ lib/so_read.c
@@ -16,6 +16,26 @@
 #include <so_header.h>
 #include <so_read.h>
 
+int so_read_startup(uint32_t *len, uint8_t **data, uint32_t *size)
+{
+	if (*size < sizeof(uint32_t))
+		return sizeof(uint32_t) - *size;
+	/* len */
+	uint32_t pos_size = *size;
+	uint8_t *pos = *data;
+	so_stream_read32(len, &pos, &pos_size);
+	uint32_t len_to_read;
+	len_to_read = *len - *size;
+	if (len_to_read > 0)
+		return len_to_read;
+	/* advance data stream */
+	*data += *len;
+	*size -= *len;
+	/* set actual data size */
+	*len  -= sizeof(uint32_t);
+	return 0;
+}
+
 int so_read(uint32_t *len, uint8_t **data, uint32_t *size)
 {
 	if (*size < sizeof(soheader_t))
@@ -31,7 +51,7 @@ int so_read(uint32_t *len, uint8_t **data, uint32_t *size)
 	/* advance data stream */
 	*data += sizeof(uint8_t) + *len;
 	*size -= sizeof(uint8_t) + *len;
-	/* get actual data size */
+	/* set actual data size */
 	*len  -= sizeof(uint32_t);
 	return 0;
 }
