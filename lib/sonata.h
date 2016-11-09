e1dd6437a75a721b8f1d9876edd4ef6ba2e36427
Dmitry Simonenko <pmwkaa@gmail.com>
2016-11-09 16:19:50 +0300

    sonata: add public import header

---
+++ lib/sonata.h
@@ -0,0 +1,24 @@
+#ifndef SONATA_H_
+#define SONATA_H_
+
+/*
+ * sonata.
+ *
+ * Protocol-level PostgreSQL client library.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+
+#include <so_macro.h>
+#include <so_stream.h>
+#include <so_header.h>
+#include <so_read.h>
+#include <so_fewrite.h>
+#include <so_bewrite.h>
+#include <so_beread.h>
+
+#endif
