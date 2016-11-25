5c4b289aea661dbe0dda34bc398da6f7bdb7b5e0
Dmitry Simonenko <pmwkaa@gmail.com>
2016-11-25 13:18:13 +0300

    flint: add mm_iobind

---
+++ machinarium/mm_iobind.c
@@ -0,0 +1,24 @@
+
+/*
+ * machinarium.
+ *
+ * Cooperative multitasking engine.
+*/
+
+#include <machinarium_private.h>
+#include <machinarium.h>
+
+MM_API int
+mm_bind(mmio_t iop, char *addr, int port)
+{
+	mmio *io = iop;
+	struct sockaddr_in saddr;
+	int rc;
+	rc = uv_ip4_addr(addr, port, &saddr);
+	if (rc < 0)
+		return rc;
+	rc = uv_tcp_bind(&io->handle, (struct sockaddr*)&saddr, 0);
+	if (rc < 0)
+		return rc;
+	return 0;
+}
