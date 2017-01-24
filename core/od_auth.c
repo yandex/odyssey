d85b592e9e814a80d79a78608f4096c65cde4b1e
Dmitry Simonenko <pmwkaa@gmail.com>
2017-01-24 14:50:02 +0300

    odissey: track od_auth files

---
+++ core/od_auth.c
@@ -0,0 +1,49 @@
+
+/*
+ * odissey.
+ *
+ * PostgreSQL connection pooler and request router.
+*/
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <machinarium.h>
+#include <soprano.h>
+
+#include "od_macro.h"
+#include "od_list.h"
+#include "od_pid.h"
+#include "od_syslog.h"
+#include "od_log.h"
+#include "od_scheme.h"
+#include "od_lex.h"
+#include "od_config.h"
+#include "od_server.h"
+#include "od_server_pool.h"
+#include "od_route_id.h"
+#include "od_route.h"
+#include "od_route_pool.h"
+#include "od_client.h"
+#include "od_client_pool.h"
+#include "od.h"
+#include "od_io.h"
+#include "od_pooler.h"
+#include "od_fe.h"
+#include "od_auth.h"
+
+int od_auth(od_client_t *client)
+{
+	so_stream_t *stream = &client->stream;
+	so_stream_reset(stream);
+	client->user = NULL;
+	int rc;
+	rc = so_bewrite_authentication(stream, 0);
+	if (rc == -1)
+		return -1;
+	rc = od_write(client->io, stream);
+	return rc;
+}
---
+++ core/od_auth.h
@@ -0,0 +1,12 @@
+#ifndef OD_AUTH_H_
+#define OD_AUTH_H_
+
+/*
+ * odissey.
+ *
+ * PostgreSQL connection pooler and request router.
+*/
+
+int od_auth(od_client_t*);
+
+#endif
