5b0a38b775695e0387043cf6cff8525fe0bfae10
Dmitry Simonenko <pmwkaa@gmail.com>
2017-02-07 18:43:36 +0300

    odissey: pass build type to machinarium

--- cmake/BuildMachinarium.cmake
+++ cmake/BuildMachinarium.cmake
@@ -4,7 +4,7 @@ macro(build_machinarium)
 	if (${PROJECT_BINARY_DIR} STREQUAL ${PROJECT_SOURCE_DIR})
 		add_custom_command(
 			OUTPUT  ${PROJECT_BINARY_DIR}/lib/machinarium/src/libmachinarium${CMAKE_STATIC_LIBRARY_SUFFIX}
-			COMMAND ${CMAKE_COMMAND} ${PROJECT_BINARY_DIR}/lib/machinarium -DBUILD_STATIC=ON
+			COMMAND ${CMAKE_COMMAND} ${PROJECT_BINARY_DIR}/lib/machinarium -DBUILD_STATIC=ON -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 			COMMAND ${CMAKE_MAKE_PROGRAM} -C ${PROJECT_BINARY_DIR}/lib/machinarium
 			WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/lib/machinarium
 		)
@@ -13,7 +13,7 @@ macro(build_machinarium)
 			OUTPUT  ${PROJECT_BINARY_DIR}/lib/machinarium/src/libmachinarium${CMAKE_STATIC_LIBRARY_SUFFIX}
 			COMMAND ${CMAKE_COMMAND} -E make_directory ${PROJECT_BINARY_DIR}/lib/machinarium
 			COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/lib/machinarium ${PROJECT_BINARY_DIR}/lib/machinarium
-			COMMAND cd ${PROJECT_BINARY_DIR}/lib/machinarium && ${CMAKE_COMMAND} -DBUILD_STATIC=ON .
+			COMMAND cd ${PROJECT_BINARY_DIR}/lib/machinarium && ${CMAKE_COMMAND} -DBUILD_STATIC=ON -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} .
 			COMMAND ${CMAKE_MAKE_PROGRAM} -C ${PROJECT_BINARY_DIR}/lib/machinarium
 		)
 	endif()
