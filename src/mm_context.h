8eda2208ef4d64f7c6ce045ca79e78b93afd2c40
Dmitry Simonenko <pmwkaa@gmail.com>
2017-04-13 20:35:49 +0300

    machinarium: make typedef of coroutine function

--- src/mm_context.h
+++ src/mm_context.h
@@ -7,9 +7,11 @@
  * cooperative multitasking engine.
 */
 
+typedef void (*mm_context_callback_t)(void*);
+
 void *mm_context_alloc(size_t);
 void  mm_context_free(void*);
-void  mm_context_create(void*, void (*)(void*), void*);
+void  mm_context_create(void*, mm_context_callback_t, void*);
 void  mm_context_swap(void*, void*);
 
 #endif
