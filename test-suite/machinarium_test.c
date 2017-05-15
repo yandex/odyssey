f0ad7a3db979d4fa2c3e1470ef50ed6d5beefdfd
Dmitry Simonenko <pmwkaa@gmail.com>
2017-05-15 17:32:22 +0300

    machinarium: add accept cancel and timeout tests

--- test-suite/machinarium_test.c
+++ test-suite/machinarium_test.c
@@ -25,6 +25,8 @@ extern void test_connect(void);
 extern void test_connect_timeout(void);
 extern void test_connect_cancel0(void);
 extern void test_connect_cancel1(void);
+extern void test_accept_timeout(void);
+extern void test_accept_cancel(void);
 
 int
 main(int argc, char *argv[])
@@ -43,5 +45,7 @@ main(int argc, char *argv[])
 	machinarium_test(test_connect_timeout);
 	machinarium_test(test_connect_cancel0);
 	machinarium_test(test_connect_cancel1);
+	machinarium_test(test_accept_timeout);
+	machinarium_test(test_accept_cancel);
 	return 0;
 }
---
+++ test-suite/test_accept_timeout.c
@@ -0,0 +1,55 @@
+
+/*
+ * machinarium.
+ *
+ * Cooperative multitasking engine.
+*/
+
+#include <machinarium.h>
+#include <machinarium_test.h>
+
+#include <arpa/inet.h>
+
+static void
+test_server(void *arg)
+{
+	machine_t machine = arg;
+
+	machine_io_t server = machine_create_io(machine);
+	test(server != NULL);
+
+	struct sockaddr_in sa;
+	sa.sin_family = AF_INET;
+	sa.sin_addr.s_addr = inet_addr("127.0.0.1");
+	sa.sin_port = htons(7778);
+	int rc;
+	rc = machine_bind(server, (struct sockaddr*)&sa);
+	test(rc == 0);
+
+	machine_io_t client;
+	rc = machine_accept(server, &client, 16, 100);
+	test(rc == -1);
+	test(machine_accept_timedout(server));
+
+	rc = machine_close(server);
+	test(rc == 0);
+
+	machine_free_io(server);
+	machine_stop(machine);
+}
+
+void
+test_accept_timeout(void)
+{
+	machine_t machine = machine_create();
+	test(machine != NULL);
+
+	int rc;
+	rc = machine_create_fiber(machine, test_server, machine);
+	test(rc != -1);
+
+	machine_start(machine);
+
+	rc = machine_free(machine);
+	test(rc != -1);
+}
