9740e070ba185abdfdce73ce0260f1ac981f45bf
Dmitry Simonenko <pmwkaa@gmail.com>
2017-05-19 14:12:00 +0300

    machinarium: implement msg pool

---
+++ src/mm_msg_pool.c
@@ -0,0 +1,59 @@
+
+/*
+ * machinarium.
+ *
+ * cooperative multitasking engine.
+*/
+
+#include <machinarium.h>
+#include <machinarium_private.h>
+
+void mm_msgpool_init(mm_msgpool_t *pool)
+{
+	pthread_spin_init(&pool->lock, PTHREAD_PROCESS_PRIVATE);
+	mm_list_init(&pool->list);
+	pool->count = 0;
+}
+
+void mm_msgpool_free(mm_msgpool_t *pool)
+{
+	mm_list_t *i, *n;
+	mm_list_foreach_safe(&pool->list, i, n) {
+		mm_msg_t *msg = mm_container_of(i, mm_msg_t, link);
+		free(msg);
+	}
+	pthread_spin_destroy(&pool->lock);
+}
+
+mm_msg_t*
+mm_msgpool_pop(mm_msgpool_t *pool)
+{
+	mm_msg_t *msg = NULL;
+	pthread_spin_lock(&pool->lock);
+	if (pool->count > 0) {
+		mm_list_t *first = mm_list_pop(&pool->list);
+		pool->count--;
+		pthread_spin_unlock(&pool->lock);
+		msg = mm_container_of(first, mm_msg_t, link);
+		goto init;
+	}
+	pthread_spin_unlock(&pool->lock);
+
+	msg = malloc(sizeof(mm_msg_t));
+	if (msg == NULL)
+		return NULL;
+init:
+	msg->refs = 0;
+	msg->type = 0;
+	msg->data = NULL;
+	mm_list_init(&msg->link);
+	return msg;
+}
+
+void mm_msgpool_push(mm_msgpool_t *pool, mm_msg_t *msg)
+{
+	pthread_spin_lock(&pool->lock);
+	mm_list_append(&pool->list, &msg->link);
+	pool->count++;
+	pthread_spin_unlock(&pool->lock);
+}
---
+++ src/mm_msg_pool.h
@@ -0,0 +1,50 @@
+#ifndef MM_MSG_POOL_H_
+#define MM_MSG_POOL_H_
+
+/*
+ * machinarium.
+ *
+ * cooperative multitasking engine.
+*/
+
+typedef struct mm_msg_t     mm_msg_t;
+typedef struct mm_msgpool_t mm_msgpool_t;
+
+struct mm_msg_t {
+	uint16_t  refs;
+	int       type;;
+	void     *data;
+	mm_list_t link;
+};
+
+struct mm_msgpool_t {
+	pthread_spinlock_t lock;
+	mm_list_t          list;
+	int                count;
+};
+
+void mm_msgpool_init(mm_msgpool_t*);
+void mm_msgpool_free(mm_msgpool_t*);
+
+mm_msg_t*
+mm_msgpool_pop(mm_msgpool_t*);
+
+void mm_msgpool_push(mm_msgpool_t*, mm_msg_t*);
+
+static inline void
+mm_msg_ref(mm_msg_t *msg)
+{
+	msg->refs++;
+}
+
+static inline void
+mm_msg_unref(mm_msgpool_t *pool, mm_msg_t *msg)
+{
+	if (msg->refs > 0) {
+		msg->refs--;
+		return;
+	}
+	mm_msgpool_push(pool, msg);
+}
+
+#endif
