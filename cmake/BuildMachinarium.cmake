87bd137618d543a56b0ee801b427efc5aac1c682
Dmitry Simonenko <pmwkaa@gmail.com>
2017-07-04 16:36:32 +0300

    odissey: move submodules from lib/ to third_party/

--- cmake/BuildMachinarium.cmake
+++ cmake/BuildMachinarium.cmake
@@ -1,27 +1,27 @@
 
 macro(build_machinarium)
-	set(MACHINARIUM_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/lib/machinarium/sources)
+	set(MACHINARIUM_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/third_party/machinarium/sources)
 	if (${PROJECT_BINARY_DIR} STREQUAL ${PROJECT_SOURCE_DIR})
 		add_custom_command(
-			OUTPUT  ${PROJECT_BINARY_DIR}/lib/machinarium/sources/libmachinarium${CMAKE_STATIC_LIBRARY_SUFFIX}
-			COMMAND ${CMAKE_COMMAND} ${PROJECT_BINARY_DIR}/lib/machinarium -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-			COMMAND ${CMAKE_MAKE_PROGRAM} -C ${PROJECT_BINARY_DIR}/lib/machinarium
-			WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/lib/machinarium
+			OUTPUT  ${PROJECT_BINARY_DIR}/third_party/machinarium/sources/libmachinarium${CMAKE_STATIC_LIBRARY_SUFFIX}
+			COMMAND ${CMAKE_COMMAND} ${PROJECT_BINARY_DIR}/third_party/machinarium -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+			COMMAND ${CMAKE_MAKE_PROGRAM} -C ${PROJECT_BINARY_DIR}/third_party/machinarium
+			WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/third_party/machinarium
 		)
 	else()
 		add_custom_command(
-			OUTPUT  ${PROJECT_BINARY_DIR}/lib/machinarium/sources/libmachinarium${CMAKE_STATIC_LIBRARY_SUFFIX}
-			COMMAND ${CMAKE_COMMAND} -E make_directory ${PROJECT_BINARY_DIR}/lib/machinarium
-			COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/lib/machinarium ${PROJECT_BINARY_DIR}/lib/machinarium
-			COMMAND cd ${PROJECT_BINARY_DIR}/lib/machinarium && ${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} .
-			COMMAND ${CMAKE_MAKE_PROGRAM} -C ${PROJECT_BINARY_DIR}/lib/machinarium
+			OUTPUT  ${PROJECT_BINARY_DIR}/third_party/machinarium/sources/libmachinarium${CMAKE_STATIC_LIBRARY_SUFFIX}
+			COMMAND ${CMAKE_COMMAND} -E make_directory ${PROJECT_BINARY_DIR}/third_party/machinarium
+			COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/third_party/machinarium ${PROJECT_BINARY_DIR}/third_party/machinarium
+			COMMAND cd ${PROJECT_BINARY_DIR}/third_party/machinarium && ${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} .
+			COMMAND ${CMAKE_MAKE_PROGRAM} -C ${PROJECT_BINARY_DIR}/third_party/machinarium
 		)
 	endif()
 	add_custom_target(libmachinarium ALL
-		DEPENDS ${PROJECT_BINARY_DIR}/lib/machinarium/sources/libmachinarium${CMAKE_STATIC_LIBRARY_SUFFIX}
+		DEPENDS ${PROJECT_BINARY_DIR}/third_party/machinarium/sources/libmachinarium${CMAKE_STATIC_LIBRARY_SUFFIX}
 	)
-	message(STATUS "Use shipped libmachinarium: ${PROJECT_SOURCE_DIR}/lib/machinarium")
-	set (MACHINARIUM_LIBRARIES "${PROJECT_BINARY_DIR}/lib/machinarium/sources/libmachinarium${CMAKE_STATIC_LIBRARY_SUFFIX}")
+	message(STATUS "Use shipped libmachinarium: ${PROJECT_SOURCE_DIR}/third_party/machinarium")
+	set (MACHINARIUM_LIBRARIES "${PROJECT_BINARY_DIR}/third_party/machinarium/sources/libmachinarium${CMAKE_STATIC_LIBRARY_SUFFIX}")
 	set (MACHINARIUM_FOUND 1)
 	add_dependencies(build_libs libmachinarium)
 endmacro(build_machinarium)
--- cmake/BuildShapito.cmake
+++ cmake/BuildShapito.cmake
@@ -1,27 +1,27 @@
 
 macro(build_shapito)
-	set(SHAPITO_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/lib/shapito/sources)
+	set(SHAPITO_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/third_party/shapito/sources)
 	if (${PROJECT_BINARY_DIR} STREQUAL ${PROJECT_SOURCE_DIR})
 		add_custom_command(
-			OUTPUT  ${PROJECT_BINARY_DIR}/lib/shapito/sources/libshapito${CMAKE_STATIC_LIBRARY_SUFFIX}
-			COMMAND ${CMAKE_COMMAND} ${PROJECT_BINARY_DIR}/lib/shapito -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-			COMMAND ${CMAKE_MAKE_PROGRAM} -C ${PROJECT_BINARY_DIR}/lib/shapito
-			WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/lib/shapito
+			OUTPUT  ${PROJECT_BINARY_DIR}/third_party/shapito/sources/libshapito${CMAKE_STATIC_LIBRARY_SUFFIX}
+			COMMAND ${CMAKE_COMMAND} ${PROJECT_BINARY_DIR}/third_party/shapito -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+			COMMAND ${CMAKE_MAKE_PROGRAM} -C ${PROJECT_BINARY_DIR}/third_party/shapito
+			WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/third_party/shapito
 		)
 	else()
 		add_custom_command(
-			OUTPUT  ${PROJECT_BINARY_DIR}/lib/shapito/sources/libshapito{CMAKE_STATIC_LIBRARY_SUFFIX}
-			COMMAND ${CMAKE_COMMAND} -E make_directory ${PROJECT_BINARY_DIR}/lib/shapito
-			COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/lib/shapito ${PROJECT_BINARY_DIR}/lib/shapito
-			COMMAND cd ${PROJECT_BINARY_DIR}/lib/shapito && ${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} .
-			COMMAND ${CMAKE_MAKE_PROGRAM} -C ${PROJECT_BINARY_DIR}/lib/shapito
+			OUTPUT  ${PROJECT_BINARY_DIR}/third_party/shapito/sources/libshapito{CMAKE_STATIC_LIBRARY_SUFFIX}
+			COMMAND ${CMAKE_COMMAND} -E make_directory ${PROJECT_BINARY_DIR}/third_party/shapito
+			COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/third_party/shapito ${PROJECT_BINARY_DIR}/third_party/shapito
+			COMMAND cd ${PROJECT_BINARY_DIR}/third_party/shapito && ${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} .
+			COMMAND ${CMAKE_MAKE_PROGRAM} -C ${PROJECT_BINARY_DIR}/third_party/shapito
 		)
 	endif()
 	add_custom_target(libshapito ALL
-		DEPENDS ${PROJECT_BINARY_DIR}/lib/shapito/sources/libshapito${CMAKE_STATIC_LIBRARY_SUFFIX}
+		DEPENDS ${PROJECT_BINARY_DIR}/third_party/shapito/sources/libshapito${CMAKE_STATIC_LIBRARY_SUFFIX}
 	)
-	message(STATUS "Use shipped libshapito: ${PROJECT_SOURCE_DIR}/lib/shapito")
-	set (SHAPITO_LIBRARIES "${PROJECT_BINARY_DIR}/lib/shapito/sources/libshapito${CMAKE_STATIC_LIBRARY_SUFFIX}")
+	message(STATUS "Use shipped libshapito: ${PROJECT_SOURCE_DIR}/third_party/shapito")
+	set (SHAPITO_LIBRARIES "${PROJECT_BINARY_DIR}/third_party/shapito/sources/libshapito${CMAKE_STATIC_LIBRARY_SUFFIX}")
 	set (SHAPITO_FOUND 1)
 	add_dependencies(build_libs libshapito)
 endmacro(build_shapito)
