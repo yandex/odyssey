f6eaa8bf2ffbab9f3da8334009c15df762881b40
Dmitry Simonenko <pmwkaa@gmail.com>
2017-08-17 16:49:37 +0300

    machinarium: check for machinarium_init during free

--- sources/mm.c
+++ sources/mm.c
@@ -8,7 +8,8 @@
 #include <machinarium.h>
 #include <machinarium_private.h>
 
-mm_t machinarium;
+static int machinarium_initialized = 0;
+mm_t       machinarium;
 
 MACHINE_API int
 machinarium_init(void)
@@ -18,12 +19,15 @@ machinarium_init(void)
 	mm_tls_init();
 	mm_taskmgr_init(&machinarium.task_mgr);
 	mm_taskmgr_start(&machinarium.task_mgr, 3);
+	machinarium_initialized = 1;
 	return 0;
 }
 
 MACHINE_API void
 machinarium_free(void)
 {
+	if (! machinarium_initialized)
+		return;
 	mm_taskmgr_stop(&machinarium.task_mgr);
 	mm_machinemgr_free(&machinarium.machine_mgr);
 	mm_msgcache_free(&machinarium.msg_cache);
