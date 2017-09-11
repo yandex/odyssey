dae2c2bd16c920a7b9d444401ac5fc7a98b349fa
Dmitry Simonenko <pmwkaa@gmail.com>
2017-09-11 17:07:55 +0300

    shapito: support password 'md5<hash>' format

--- shapito/password.c
+++ shapito/password.c
@@ -20,20 +20,26 @@ int shapito_password_md5(shapito_password_t *pw,
 	uint8_t digest_prepare[16];
 	char    digest_prepare_sz[32];
 	uint8_t digest[16];
-
-	/* digest = md5(password, user) */
 	shapito_md5_t ctx;
-	shapito_md5_init(&ctx);
-	shapito_md5_update(&ctx, password, password_len);
-	shapito_md5_update(&ctx, user, user_len);
-	shapito_md5_final(&ctx, digest_prepare);
-	shapito_md5_tostring(digest_prepare_sz, digest_prepare);
-
-	/* digest = md5(digest, salt) */
-	shapito_md5_init(&ctx);
-	shapito_md5_update(&ctx, digest_prepare_sz, 32);
-	shapito_md5_update(&ctx, salt, 4);
-	shapito_md5_final(&ctx, digest);
+	if (password_len == 35 && memcmp(password, "md5", 3) == 0) {
+		/* digest = md5(digest, salt) */
+		shapito_md5_init(&ctx);
+		shapito_md5_update(&ctx, password + 3, 32);
+		shapito_md5_update(&ctx, salt, 4);
+		shapito_md5_final(&ctx, digest);
+	} else {
+		/* digest = md5(password, user) */
+		shapito_md5_init(&ctx);
+		shapito_md5_update(&ctx, password, password_len);
+		shapito_md5_update(&ctx, user, user_len);
+		shapito_md5_final(&ctx, digest_prepare);
+		shapito_md5_tostring(digest_prepare_sz, digest_prepare);
+		/* digest = md5(digest, salt) */
+		shapito_md5_init(&ctx);
+		shapito_md5_update(&ctx, digest_prepare_sz, 32);
+		shapito_md5_update(&ctx, salt, 4);
+		shapito_md5_final(&ctx, digest);
+	}
 
 	/* 'md5' + to_string(digest) */
 	pw->password_len = 35 + 1;
