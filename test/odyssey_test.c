8518e9a959a0c7038d4a8ed27ce50bfda67f6698
Dmitry Simonenko <pmwkaa@gmail.com>
2018-04-04 16:36:11 +0300

    odyssey: add initial odyssey test suite stub

---
+++ test/odyssey_test.c
@@ -0,0 +1,23 @@
+
+/*
+ * Odyssey.
+ *
+ * Scalable PostgreSQL connection pooler.
+*/
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+
+#include <machinarium.h>
+#include <shapito.h>
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	return 0;
+}
