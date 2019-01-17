a8428db0bc6f9b5b0b31fa16f13f3b56bed49fa4
Andrey Borodin <amborodin@acm.org>
2019-01-17 15:52:02 +0300

    Move to scripts folder

--- scripts/pgsql_benchmark.sh
+++ scripts/pgsql_benchmark.sh
@@ -52,7 +52,7 @@ trap cleanup ERR INT TERM
 echo "Make temp build"
 mkdir tmpbuild
 cd tmpbuild
-cmake .. >/dev/null
+cmake -DCMAKE_BUILD_TYPE=Release ../.. >/dev/null
 make -j 4 >/dev/null
 
 echo "Make temp DB"
@@ -60,11 +60,23 @@ $PGINSTALL/bin/initdb datadir >/dev/null
 $PGINSTALL/bin/pg_ctl -D datadir start >/dev/null
 
 echo "Start Odyssey"
-./sources/odyssey ../odyssey.conf>/dev/null &
+./sources/odyssey ../../odyssey.conf>/dev/null &
 cd ..
 
+sleep 1
 
-PGHOST=127.0.0.1 PGPORT=6432 make -C $PGSRC installcheck
+#suppress pgbench warnings in benchmarks loop
+PGHOST=127.0.0.1 PGPORT=6432 $PGINSTALL/bin/pgbench -i postgres
+
+declare -a resultset_sizes=("1" "100" "10000" "10000000")
+
+## now loop through the above array
+for size in "${resultset_sizes[@]}"
+do
+    echo "resultset_size $size"
+    echo "select generate_series(1,$size);"> tmpbuild/shot.sql
+    PGHOST=127.0.0.1 PGPORT=6432 $PGINSTALL/bin/pgbench --no-vacuum -T 10 -c 4 -j 4 -f tmpbuild/shot.sql postgres| grep " = "
+done
 
 echo "Stop Odyssey"
 kill %1
