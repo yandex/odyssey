9b56ff4ec1eb2e6c17259f25af096618a21f6921
Andrey Borodin <amborodin@acm.org>
2020-02-17 11:03:13 +0500

    Test for NULL in tests

--- test/machinarium/test_producer_consumer1.c
+++ test/machinarium/test_producer_consumer1.c
@@ -13,6 +13,7 @@ test_consumer(void *arg)
 	for (;;) {
 		machine_msg_t *msg;
 		msg = machine_channel_read(channel, UINT32_MAX);
+        test(msg != NULL);
 		consumers_stat[consumer_id]++;
 		int is_exit = (uint32_t)machine_msg_type(msg) == UINT32_MAX;
 		machine_msg_free(msg);
