bfd5e07ed28c6d5669c669c09e7113a9d74ba658
kirill reshke <reshkekirill@gmail.com>
2020-06-16 10:27:33 +0500

    PAM PI modifications (#176)
    Co-authored-by: reshke <Kirill Reshke>

--- sources/pam.h
+++ sources/pam.h
@@ -9,7 +9,7 @@
 
 struct od_pam_auth_data
 {
-	int key;
+	int msg_style;
 	char *value;
 	od_list_t link;
 };
@@ -18,11 +18,12 @@ typedef struct od_pam_auth_data od_pam_auth_data_t;
 
 int
 od_pam_auth(char *od_pam_service,
+            char *usrname,
             od_pam_auth_data_t *auth_data,
             machine_io_t *io);
 
 void
-od_pam_convert_usr_passwd(od_pam_auth_data_t *d, char *usr, char *passwd);
+od_pam_convert_passwd(od_pam_auth_data_t *d, char *passwd);
 
 od_pam_auth_data_t *
 od_pam_auth_data_create(void);
