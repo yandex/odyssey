71b61798f13bb93521a18f0f959e38d19386c627
reshke <Kirill Reshke>
2020-08-03 17:06:27 +0500

    fix cmake for version

--- sources/build.h.cmake
+++ sources/build.h.cmake
@@ -9,11 +9,6 @@
 
 /* AUTO-GENERATED (see version.h.cmake) */
 
-#cmakedefine OD_VERSION_GIT "@OD_VERSION_GIT@"
-#cmakedefine OD_VERSION_BUILD "@OD_VERSION_BUILD@"
-
-#cmakedefine OD_VERSION_NUMBER "@OD_VERSION_NUMBER@"
-
 #cmakedefine PAM_FOUND 1
 #cmakedefine PG_VERSION_NUM @PG_VERSION_NUM @
 
