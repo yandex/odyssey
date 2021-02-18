a3e7ed62329ac26873fd6f195960d09da7805af9
Marc Rechté <marc4@rechte.fr>
2021-02-18 15:42:37 +0100

    Fix again code formatting issue.

--- sources/instance.c
+++ sources/instance.c
@@ -184,9 +184,8 @@ int od_instance_main(od_instance_t *instance, int argc, char **argv)
 		rc = od_pid_create(&instance->pid, instance->config.pid_file);
 		if (rc == -1) {
 			od_error(&instance->logger, "init", NULL, NULL,
-			"failed to create pid file %s: %s",
-			instance->config.pid_file,
-			strerror(errno));
+				 "failed to create pid file %s: %s",
+				 instance->config.pid_file, strerror(errno));
 			goto error;
 		}
 	}
