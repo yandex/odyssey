5e0fff1205f4b405fc43ad2a59172ff3986feef5
reshke <reshkekirill@gmail.com>
2021-11-12 17:04:03 +0500

    Fix fmt issue

--- sources/tls.c
+++ sources/tls.c
@@ -88,7 +88,8 @@ int od_tls_frontend_accept(od_client_t *client, od_logger_t *logger,
 		}
 
 		if (od_readahead_unread(&client->io.readahead) > 0) {
-			od_error(logger, "tls", client, NULL, "extraneous data from client");
+			od_error(logger, "tls", client, NULL,
+				 "extraneous data from client");
 			return -1; // prevent possible buffer, protecting against CVE-2021-23214-like attacks
 		}
 
@@ -190,10 +191,11 @@ int od_tls_backend_connect(od_server_t *server, od_logger_t *logger,
 	case 'S':
 		/* supported */
 		od_debug(logger, "tls", NULL, server, "supported");
-                if (od_readahead_unread(&server->io.readahead) > 0) {
-			od_error(logger, "tls", NULL, server, "extraneous data from client");
-                        return -1; // prevent possible buffer, protecting against CVE-2021-23214-like attacks
-                }
+		if (od_readahead_unread(&server->io.readahead) > 0) {
+			od_error(logger, "tls", NULL, server,
+				 "extraneous data from client");
+			return -1; // prevent possible buffer, protecting against CVE-2021-23214-like attacks
+		}
 
 		rc = machine_set_tls(server->io.io, server->tls, UINT32_MAX);
 		if (rc == -1) {
