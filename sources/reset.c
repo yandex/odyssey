3f01969f3161b985fac15252e6cd0cccbaf8bd19
Andrey Zhidenkov <pensnarik@gmail.com>
2023-04-19 14:54:35 +0300

    Add logging for smart discard query

--- sources/reset.c
+++ sources/reset.c
@@ -131,19 +131,23 @@ int od_reset(od_server_t *server)
 
 	/* send smart discard */
 	if (route->rule->pool->smart_discard) {
-		if (route->rule->pool->discard_string) {
-			rc = od_backend_query(
-				server, "reset-discard-smart-string",
-				route->rule->pool->discard_string, NULL,
-				sizeof(route->rule->pool->discard_string),
-				wait_timeout, 1);
-		} else {
+		if (route->rule->pool->discard_string == NULL) {
 			char query_discard[] =
 				"SET SESSION AUTHORIZATION DEFAULT;RESET ALL;CLOSE ALL;UNLISTEN *;SELECT pg_advisory_unlock_all();DISCARD PLANS;DISCARD SEQUENCES;DISCARD TEMP;";
 			rc = od_backend_query(server, "reset-discard-smart",
 					      query_discard, NULL,
 					      sizeof(query_discard),
 					      wait_timeout, 1);
+		} else {
+			od_log(&instance->logger, "reset",
+			       server->client, server,
+			       "smart discard query is %s", route->rule->pool->discard_string);
+
+			rc = od_backend_query(
+				server, "reset-discard-smart-string",
+				route->rule->pool->discard_string, NULL,
+				sizeof(route->rule->pool->discard_string),
+				wait_timeout, 1);
 		}
 		if (rc == NOT_OK_RESPONSE)
 			goto error;
