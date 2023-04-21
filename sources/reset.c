597b503963f35edf4e7db0db868a4ed1d6400970
Andrey Zhidenkov <pensnarik@gmail.com>
2023-04-21 21:24:58 +0300

    Discard string is always executed if set no matter if smart_discard is enabled

--- sources/reset.c
+++ sources/reset.c
@@ -130,21 +130,22 @@ int od_reset(od_server_t *server)
 	}
 
 	/* send smart discard */
-	if (route->rule->pool->smart_discard) {
-		if (route->rule->pool->discard_string == NULL) {
-			char query_discard[] =
-				"SET SESSION AUTHORIZATION DEFAULT;RESET ALL;CLOSE ALL;UNLISTEN *;SELECT pg_advisory_unlock_all();DISCARD PLANS;DISCARD SEQUENCES;DISCARD TEMP;";
-			rc = od_backend_query(server, "reset-discard-smart",
-					      query_discard, NULL,
-					      sizeof(query_discard),
-					      wait_timeout, 1);
-		} else {
-			rc = od_backend_query(
-				server, "reset-discard-smart-string",
-				route->rule->pool->discard_string, NULL,
-				strlen(route->rule->pool->discard_string) + 1,
-				wait_timeout, 1);
-		}
+	if (route->rule->pool->smart_discard &&
+	    route->rule->pool->discard_string == NULL) {
+		char query_discard[] =
+			"SET SESSION AUTHORIZATION DEFAULT;RESET ALL;CLOSE ALL;UNLISTEN *;SELECT pg_advisory_unlock_all();DISCARD PLANS;DISCARD SEQUENCES;DISCARD TEMP;";
+		rc = od_backend_query(server, "reset-discard-smart",
+				      query_discard, NULL,
+				      sizeof(query_discard), wait_timeout, 1);
+		if (rc == NOT_OK_RESPONSE)
+			goto error;
+	}
+	if (route->rule->pool->discard_string != NULL) {
+		rc = od_backend_query(
+			server, "reset-discard-smart-string",
+			route->rule->pool->discard_string, NULL,
+			strlen(route->rule->pool->discard_string) + 1,
+			wait_timeout, 1);
 		if (rc == NOT_OK_RESPONSE)
 			goto error;
 	}
