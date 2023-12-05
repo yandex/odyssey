bfb8f119740c9f873f503070bd851ee1e1fbbddd
nikunis <nik.unisikhin.05@gmail.com>
2023-12-05 17:25:12 +0500

    fix

--- sources/address.h
+++ sources/address.h
@@ -15,10 +15,6 @@ int od_address_read(struct sockaddr_storage *, const char *);
 bool od_address_inet_compare(struct sockaddr_storage *,
 			     struct sockaddr_storage *);
 
-uint32 od_address_bswap32(uint32 x)
-{
-	return ((x << 24) & 0xff000000) | ((x << 8) & 0x00ff0000) |
-	       ((x >> 8) & 0x0000ff00) | ((x >> 24) & 0x000000ff);
-}
+uint32 od_address_bswap32(uint32);
 
 #endif /* ODYSSEY_ADDRESS_H */
