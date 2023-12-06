157da25efd6495742194596470496ce63a0e3679
nikunis <nik.unisikhin.05@gmail.com>
2023-12-06 19:01:42 +0500

    add tests for rules with addresses

---
+++ docker/rule-address/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+
+set -ex
+
+/usr/bin/odyssey /rule-address/addr.conf
+
+PGPASSWORD=correct_password psql -h localhost -p 6432 -U user_addr_correct -c "SELECT 1" addr_db > /dev/null 2>&1 || {
+  echo "ERROR: failed auth with correct addr, correct password and plain password in config"
+
+	cat /var/log/odyssey.log
+
+	exit 1
+}
+
+PGPASSWORD=incorrect_password psql -h localhost -p 6432 -U user_addr_correct -c "SELECT 1" addr_db > /dev/null 2>&1 && {
+  echo "ERROR: successfully auth with correct addr, but incorrect password"
+
+	cat /var/log/odyssey.log
+
+	exit 1
+}
+
+PGPASSWORD=correct_password psql -h localhost -p 6432 -U user_addr_incorrect -c "SELECT 1" addr_db > /dev/null 2>&1 && {
+  echo "ERROR: successfully auth with incorrect addr"
+
+	cat /var/log/odyssey.log
+
+	exit 1
+}
+
+ody-stop
