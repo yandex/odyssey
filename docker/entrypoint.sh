b54e5b66e8a14544658d105d19cb60b85c25698d
nikunis <nik.unisikhin.05@gmail.com>
2023-12-08 14:02:13 +0500

    start debug

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,11 +11,11 @@ then
 	exit 1
 fi
 
-# auth query
-/auth_query/test_auth_query.sh
-if [ $? -eq 1 ]
-then
-	exit 1
-fi
+#prepared statements in transaction pooling
+/usr/bin/odyssey /etc/odyssey/pstmts.conf
+sleep 1
+/pstmts-test
+
+ody-stop
 
 teardown
