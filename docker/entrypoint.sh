2f290a4884a6477f9f871c22320789871338aab1
nikunis <nik.unisikhin.05@gmail.com>
2023-12-08 17:38:20 +0500

    debug

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,13 +4,6 @@ set -ex
 
 setup
 
-# odyssey rule-address test
-/rule-address/test.sh
-if [ $? -eq 1 ]
-then
-	exit 1
-fi
-
 #prepared statements in transaction pooling
 /usr/bin/odyssey /etc/odyssey/pstmts.conf
 sleep 1
