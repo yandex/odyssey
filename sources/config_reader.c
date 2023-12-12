e7c3ce231afd95ad8712b41b72e75f883a119c2c
nikunis <nik.unisikhin.05@gmail.com>
2023-12-12 18:14:21 +0500

    fix

--- sources/config_reader.c
+++ sources/config_reader.c
@@ -1729,6 +1729,7 @@ static int od_config_reader_route(od_config_reader_t *reader, char *db_name,
 
 	od_address_range_t address_range;
 	address_range.string = NULL;
+	address_range.is_default = 0;
 
 	if (od_config_reader_is(reader, OD_PARSER_STRING)) {
 		if (!od_config_reader_string(reader, &address_range.string))
