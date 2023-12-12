a2095a85201dd7117f95117a1567b0e10aa7029c
nikunis <nik.unisikhin.05@gmail.com>
2023-12-12 16:54:24 +0500

    start replaced addr_mask_is_default to is_default

--- sources/rules.h
+++ sources/rules.h
@@ -70,9 +70,6 @@ struct od_rule {
 	char *user_name;
 	int user_name_len;
 	int user_is_default;
-	struct sockaddr_storage addr;
-	struct sockaddr_storage mask;
-	int addr_mask_is_default;
 	od_address_range_t address_range;
 	od_rule_role_type_t user_role;
 
