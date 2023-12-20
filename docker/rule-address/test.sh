d071192a532b07a3ff2bffd4fc08347f8b464b7f
nikunis <nik.unisikhin.05@gmail.com>
2023-12-20 18:23:55 +0500

    add tests

--- docker/rule-address/test.sh
+++ docker/rule-address/test.sh
@@ -60,4 +60,20 @@ PGPASSWORD=incorrect_password psql -h localhost -p 6432 -U user_addr_empty -c "S
 	exit 1
 }
 
+PGPASSWORD=correct_password psql -h localhost -p 6432 -U user_addr_hostname_localhost -c "SELECT 1" addr_db > /dev/null 2>&1 || {
+  echo "ERROR: failed auth with correct addr, correct password and plain password in config"
+
+	cat /var/log/odyssey.log
+
+	exit 1
+}
+
+PGPASSWORD=incorrect_password psql -h localhost -p 6432 -U user_addr_hostname_localhost -c "SELECT 1" addr_db > /dev/null 2>&1 && {
+  echo "ERROR: successfully auth with correct addr, but incorrect password"
+
+	cat /var/log/odyssey.log
+
+	exit 1
+}
+
 ody-stop
