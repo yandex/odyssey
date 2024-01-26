b55ca49eac49e43a4dd209bcfb633b25029a1ad5
Andrey Ovchinnikov <ch1nn1y@yandex-team.ru>
2024-01-26 13:09:48 +0300

    remove some functions from mdb_iamproxy header file

--- sources/mdb_iamproxy.h
+++ sources/mdb_iamproxy.h
@@ -7,8 +7,6 @@
  * Scalable PostgreSQL connection pooler.
  */
 
-int mdb_iamproxy_recv_from_socket(int socket_fd, char *msg_body);
-int mdb_iamproxy_send_to_socket(int socket_fd, const char *send_msg);
 int mdb_iamproxy_authenticate_user(const char *username, const char *token, od_instance_t *instance, od_client_t *client);
 
 #endif /* ODYSSEY_IAMPROXy_H */
