6a2229e3acc9c4210df678bc3f45f64ea61b15e2
NikitaUnisikhin <nik.unisikhin.05@gmail.com>
2024-02-08 12:52:16 +0000

    refactoring and add connection retry

--- docker/inverse_prioriteis_benchmark/pkg/main.go
+++ docker/inverse_prioriteis_benchmark/pkg/main.go
@@ -11,22 +11,26 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const clientsCount = 1000
-const connectTimeoutSec = 2
+const (
+	hostname     = "127.0.0.1"
+	odyPort      = 6432
+	databaseName = "ipb_db"
+	clientsCount = 1000
+	connectTimeoutSec = 2
+	connectRetryCount = 3
+)
 
-func connect(successConnect *int, i int, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	userName := fmt.Sprintf("inverse_priorities_benchmark_%d", i)
-	pgConString := fmt.Sprintf("postgres://%s@127.0.0.1:6432/ipb_db?sslmode=require&connect_timeout=%d",
-		userName, connectTimeoutSec)
+func connectRetry(clientNumber int) int {
+	userName := fmt.Sprintf("inverse_priorities_benchmark_%d", clientNumber)
+	pgConString := fmt.Sprintf("postgres://%s@%s:%d/%s?sslmode=require&connect_timeout=%d",
+		userName, hostname, odyPort, databaseName, connectTimeoutSec)
 	config, err := pgx.ParseConfig(pgConString)
 	if err != nil {
 		fmt.Println("DCN error:", err.Error())
-		return
+		return 1
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), connectTimeoutSec*time.Second)
 	defer cancel()
 
 	conn, err := pgx.ConnectConfig(ctx, config)
@@ -36,25 +40,34 @@ func connect(successConnect *int, i int, wg *sync.WaitGroup) {
 		} else {
 			fmt.Println("Connect error:", err.Error())
 		}
-		return
+		return 1
 	}
 
 	defer conn.Close(ctx)
-	*successConnect++
+	return 0
+}
+
+func connect(successConnect *int, clientNumber int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < connectRetryCount; i++ {
+		statusCode := connectRetry(clientNumber)
+		if statusCode == 0 {
+			*successConnect++
+			break
+		}
+	}
 }
 
 func main() {
 	fmt.Printf("inverse_prioriteis_benchmark_start\n")
 	successConnect := 0
-	connectCount := clientsCount 
 	var wg sync.WaitGroup
-	wg.Add(connectCount)
-	for i := 0; i < connectCount; i++ {
-		client_number := i % clientsCount
-		fmt.Printf("inverse_prioriteis_benchmark_connect_%d\n", client_number)
-		go connect(&successConnect, client_number, &wg)
+	wg.Add(clientsCount)
+	for i := 0; i < clientsCount; i++ {
+		fmt.Printf("inverse_prioriteis_benchmark_connect_%d\n", i)
+		go connect(&successConnect, i, &wg)
 	}
 	wg.Wait()
-	resultPercent := (float64(successConnect) / float64(connectCount)) * 100
-	fmt.Printf("inverse_prioriteis_benchmark_result: %d success connections out of %d (%.2f %%)\n", successConnect, connectCount, resultPercent)
+	resultPercent := (float64(successConnect) / float64(clientsCount)) * 100
+	fmt.Printf("inverse_prioriteis_benchmark_result: %d success connections out of %d (%.2f %%)\n", successConnect, clientsCount, resultPercent)
 }
