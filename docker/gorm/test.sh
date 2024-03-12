b9a027aeea62033ec01b79fdeed980a588dd439d
NikitaUnisikhin <nik.unisikhin.05@gmail.com>
2024-03-12 20:25:54 +0000

    refactoring, change docker-compose-test

--- docker/gorm/test.sh
+++ docker/gorm/test.sh
@@ -1,4 +1,4 @@
-docker network create gorm_test
-docker network connect gorm_test odyssey_odyssey_1
-docker build -t gorm-tests /gorm/gorm-spqr
-docker run -e DB_HOST='odyssey' -e DB_PORT=6432 -e DB_USER='spqr-console' -e DB_NAME='spqr-console' --network=gorm_test gorm-tests
+# docker run -e DB_HOST='odyssey' -e DB_PORT=6432 -e DB_USER='spqr-console' -e DB_NAME='spqr-console' --network=odyssey_od_net gorm-tests
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+COMPOSE_PROJECT_NAME=odyssey docker-compose -f /docker-compose-test.yml up gorm-test
