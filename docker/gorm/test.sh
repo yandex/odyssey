8446d131aff2c7b40d56a08d4558c7d56fedb71e
NikitaUnisikhin <nik.unisikhin.05@gmail.com>
2024-03-13 14:20:13 +0000

    rm gorm/gorm-spqr

--- docker/gorm/test.sh
+++ docker/gorm/test.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/pg-sharding/gorm-spqr.git
 cd gorm-spqr/
 docker build -t gorm-tests .
-docker run -e DB_HOST='localhost' -e DB_PORT=6432 -e DB_USER='spqr-console' -e DB_NAME='spqr-console' --network=host gorm-tests
+docker run -e DB_HOST='host.docker.internal' -e DB_PORT=6432 -e DB_USER='spqr-console' -e DB_NAME='spqr-console' --network=host gorm-tests
