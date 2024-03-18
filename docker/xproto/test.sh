8a2d7c10810d08e6f8924c1bfaf44856c6537cb5
NikitaUnisikhin <nik.unisikhin.05@gmail.com>
2024-03-18 07:27:02 +0000

    fix

--- docker/xproto/test.sh
+++ docker/xproto/test.sh
@@ -2,4 +2,4 @@ git clone https://github.com/pg-sharding/spqr.git /xproto/spqr
 cp /xproto/spqr/test/xproto/proto_test.go /xproto/proto_test.go
 rm -rf /xproto/spqr
 docker build -t xproto-tests /xproto
-docker run -e POSTGRES_HOST=localhost -e POSTGRES_PORT=5432 -e POSTGRES_DB=xproto_db -e POSTGRES_USER=xproto --network=host xproto-tests
+docker run -e POSTGRES_HOST=odyssey -e POSTGRES_PORT=6432 -e POSTGRES_DB=xproto_db -e POSTGRES_USER=xproto --network=odyssey_od_net xproto-tests
