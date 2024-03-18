5afa71b8c5871c59f1abe023aff729ec935f5ce4
NikitaUnisikhin <nik.unisikhin.05@gmail.com>
2024-03-18 11:00:54 +0000

    fix

--- docker/gorm/gorm-spqr/models/person.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Person struct {
-	ID        uint32 `json:"id" gorm:"primary_key"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-}
--- docker/gorm/gorm-spqr/utils/utils.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "github.com/pg-sharding/gorm-spqr/models"
-
-func DeleteAll() error {
-	return models.DB.Migrator().DropTable(&models.Person{})
-}
