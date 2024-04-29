e23fc563c6318b1dc4d0f923f45fe36768e73677
reshke <reshke@double.cloud>
2024-04-29 20:02:05 +0500

    Update frontend.c Fixes (#608)
    * Update frontend.c Fixes
    
    * fix

--- sources/frontend.c
+++ sources/frontend.c
@@ -1670,8 +1670,9 @@ static od_frontend_status_t od_frontend_remote(od_client_t *client)
 		// are we requested to meet sync point?
 
 		if (sync_req) {
-			od_log(&instance->logger, "sync-point", client, server,
-			       "process, %d", od_server_synchronized(server));
+			od_debug(&instance->logger, "sync-point", client,
+				 server, "process, %d",
+				 od_server_synchronized(server));
 
 			while (1) {
 				if (od_server_synchronized(server)) {
@@ -1679,8 +1680,8 @@ static od_frontend_status_t od_frontend_remote(od_client_t *client)
 				}
 				// await here
 
-				od_log(&instance->logger, "sync-point", client,
-				       server, "process await");
+				od_debug(&instance->logger, "sync-point-await",
+					 client, server, "process await");
 				status = od_frontend_remote_process_server(
 					server, client, true);
 
@@ -1690,6 +1691,8 @@ static od_frontend_status_t od_frontend_remote(od_client_t *client)
 			}
 
 			if (status != OD_OK) {
+				od_log(&instance->logger, "sync-point", client,
+				       server, "failed to meet sync point");
 				break;
 			}
 
@@ -1699,7 +1702,7 @@ static od_frontend_status_t od_frontend_remote(od_client_t *client)
 
 			/* fill internals structs in */
 			if (od_frontend_deploy_prepared_stmt_msg(
-				    server, &server->relay, "sync-point",
+				    server, &server->relay, "sync-point-deploy",
 				    server->parse_msg) != OD_OK) {
 				status = OD_ESERVER_WRITE;
 				break;
