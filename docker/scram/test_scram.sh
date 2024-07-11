5f55858cba79addd97f9f768c0839c2454e06114
Victor <victorkremensky@mail.ru>
2024-07-11 13:35:47 +0000

    Now when the test drops, the tests will drop

--- docker/scram/test_scram.sh
+++ docker/scram/test_scram.sh
@@ -3,6 +3,14 @@
 /usr/bin/odyssey /scram/config.conf
 
 /scram/test_scram_backend.sh
+if [ $? -eq 1 ]
+then
+	exit 1
+fi
 /scram/test_scram_frontend.sh
+if [ $? -eq 1 ]
+then
+	exit 1
+fi
 
 ody-stop
