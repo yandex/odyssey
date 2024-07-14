e1e77b11fb036f478c4de215e49ea4cce6e48053
Victor <87538976+visill@users.noreply.github.com>
2024-07-14 17:59:16 +0800

    Fix logic in tests (#628)
    * Now pg_hba rules apply
    
    * Now when the test drops, the tests will drop
    
    * fix tests

--- docker/scram/test_scram_backend.sh
+++ docker/scram/test_scram_backend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-psql -h localhost -p 6432 -U backend_auth_with_incorrect_password -c "SELECT 1" scram_db >/dev/null 2>&1 && {
+psql -h ip4-localhost -p 6432 -U backend_auth_with_incorrect_password -c "SELECT 1" scram_db >/dev/null 2>&1 && {
         echo "ERROR: successfully backend auth with incorrect password"
 
         cat /var/log/odyssey.log
@@ -13,7 +13,7 @@ psql -h localhost -p 6432 -U backend_auth_with_incorrect_password -c "SELECT 1" 
         exit 1
 }
 
-psql -h localhost -p 6432 -U backend_auth_with_correct_password -c "SELECT 1" scram_db >/dev/null 2>&1 || {
+psql -h ip4-localhost -p 6432 -U backend_auth_with_correct_password -c "SELECT 1" scram_db >/dev/null 2>&1 || {
         echo "ERROR: failed backend auth with correct password"
 
         cat /var/log/odyssey.log
