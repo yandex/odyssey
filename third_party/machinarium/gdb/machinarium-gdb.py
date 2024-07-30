5c573c09a950b9c68597e67e40708eba39077432
reshke <reshke@double.cloud>
2024-07-30 11:08:20 +0500

    Update machinarium-gdb.py: add ignore-errors

--- third_party/machinarium/gdb/machinarium-gdb.py
+++ third_party/machinarium/gdb/machinarium-gdb.py
@@ -489,8 +489,25 @@ Example:
                     self._execute_in_coroutine_context(thread, coro, gdbcmd)
 
 
+class IgnoreErrorsCmd (gdb.Command):
+    """Execute a single command, ignoring all errors.
+Only one-line commands are supported.
+This is primarily useful in scripts."""
+
+    def __init__ (self):
+        super (IgnoreErrorsCommand, self).__init__ ("ignore-errors",
+                                                    gdb.COMMAND_OBSCURE,
+                                                    # FIXME...
+                                                    gdb.COMPLETE_COMMAND)
+
+    def invoke (self, arg, from_tty):
+        try:
+            gdb.execute (arg, from_tty)
+        except:
+            pass
+
 MMCoroutines()
 MMCoroutineCmd()
-
+IgnoreErrorsCmd ()
 
 gdb.write('done.\n', stream=gdb.STDLOG)
