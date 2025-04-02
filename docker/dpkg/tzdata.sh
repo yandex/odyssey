05b7926e0c5bc0a6f7f79da956573944d9adf115
rkhapov <r.khapov@ya.ru>
2025-04-02 05:57:27 +0000

    docker/dpkg/tzdata.sh: remove
    It can be replaced with code inside Dockerfile
    
    Signed-off-by: rkhapov <r.khapov@ya.ru>

--- docker/dpkg/tzdata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DEBIAN_FRONTEND=noninteractive
-#install tzdata package
-# set your timezone
-ln -fs /usr/share/zoneinfo/$TZ /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
