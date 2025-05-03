7e9b16665705a3f135928d8544655b6775957678
Roman Khapov <r.khapov@yandex.ru>
2025-05-03 13:49:54 +0500

    sources/frontend.c: fix od_eject_conn_with_rate (#804)
    1) Get time by machine time functions to reduce overhead of syscalls
    2) Remove log about too early ejection - it produces too many garbage
       messages in logs
    
    Signed-off-by: rkhapov <r.khapov@ya.ru>

--- sources/ejection.h
+++ sources/ejection.h
@@ -10,7 +10,7 @@
 /* only for online restart */
 typedef struct {
 	pthread_mutex_t mu;
-	int last_conn_drop_ts;
+	uint32_t last_conn_drop_ts;
 } od_conn_eject_info;
 
 extern od_retcode_t od_conn_eject_info_init(od_conn_eject_info **dst);
