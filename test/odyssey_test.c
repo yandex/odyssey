b4c8264522d0167e3cbfa544be487aa7474f3dee
Vikmalakhov <Varman056@yandex.ru>
2025-05-20 09:37:41 +0000

    test/odyssey: Add tests to odyssey hashmap
    For future moving hashmap from odyssey to machinarium
    
    Add few tests to odyssey hashmap by inserting and lock keys

--- test/odyssey_test.c
+++ test/odyssey_test.c
@@ -106,6 +106,7 @@ extern void odyssey_test_util(void);
 extern void odyssey_test_lock(void);
 extern void odyssey_test_hba(void);
 extern void odyssey_test_endpoints_parse(void);
+extern void odyssey_test_hashmap(void);
 
 int main(int argc, char *argv[])
 {
@@ -202,6 +203,7 @@ int main(int argc, char *argv[])
 	odyssey_test(odyssey_test_lock);
 	odyssey_test(odyssey_test_hba);
 	odyssey_test(odyssey_test_endpoints_parse);
+	odyssey_test(odyssey_test_hashmap);
 
 	return 0;
 }
