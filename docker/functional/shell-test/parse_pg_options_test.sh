f0f321caa9ac410f00a459334a0248fd829f7cd4
Vikmalakhov <Varman056@yandex.ru>
2025-05-27 13:34:17 +0000

    docker/functional/shell-test/parse_pg_options_test.sh: aded tests to search_path
    Added few tests to pgoptions with many search_path arguments

--- docker/functional/shell-test/parse_pg_options_test.sh
+++ docker/functional/shell-test/parse_pg_options_test.sh
@@ -35,3 +35,13 @@ PGOPTIONS="-c search_path=tpath -c statement_timeout=5min -c      " psql "host=l
 PGOPTIONS="-c search_path=tpath -c statement_timeout=5min -c lock_timeout=invalid" psql "host=localhost port=6432 dbname=postgres user=postgres" -c 'select 1'
 PGOPTIONS="-c search_path=tpath -c statement_timeout=invalid -c lock_timeout=10s" psql "host=localhost port=6432 dbname=postgres user=postgres" -c 'select 1'
 PGOPTIONS="-c search_path=invalid -c statement_timeout=5min -c lock_timeout=10s" psql "host=localhost port=6432 dbname=postgres user=postgres" -c 'select 1'
+
+# tests for many search_path parametrs
+PGOPTIONS=--search_path=pgstac,public psql "host=localhost port=6432 dbname=postgres user=postgres" -t -c 'show search_path' | grep -E "^ *pgstac, *public *$"
+PGOPTIONS="--search_path=pgstac,public" psql "host=localhost port=6432 dbname=postgres user=postgres" -t -c 'show search_path' | grep -E "^ *pgstac, *public *$"
+PGOPTIONS=--search_path="pgstac,public" psql "host=localhost port=6432 dbname=postgres user=postgres" -t -c 'show search_path' | grep -E "^ *pgstac, *public *$"
+PGOPTIONS='-c search_path=pgstac,public' psql "host=localhost port=6432 dbname=postgres user=postgres" -t -c 'show search_path' | grep -E "^ *pgstac, *public *$"
+PGOPTIONS='-c search_path=pgstac,public,test' psql "host=localhost port=6432 dbname=postgres user=postgres" -t -c 'show search_path' | grep -E "^ *pgstac, *public, *test *$"
+PGOPTIONS='-c search_path="pgstac",public' psql "host=localhost port=6432 dbname=postgres user=postgres" -t -c 'show search_path' | grep -E "^ *\"pgstac\", *public *$"
+PGOPTIONS='-c search_path=pgstac,"public"' psql "host=localhost port=6432 dbname=postgres user=postgres" -t -c 'show search_path' | grep -E "^ *pgstac, *\"public\" *$"
+PGOPTIONS='-c search_path=pgstac -c search_path=public' psql "host=localhost port=6432 dbname=postgres user=postgres" -t -c 'show search_path' | grep -E "^ *public *$"
