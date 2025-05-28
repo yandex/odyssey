759dff5c481c4b69adbea233a19e01f31362751e
rkhapov <r.khapov@ya.ru>
2025-05-28 16:02:12 +0000

    fix
    Signed-off-by: rkhapov <r.khapov@ya.ru>

--- docker/functional/test_functional.py
+++ docker/functional/test_functional.py
@@ -31,23 +31,21 @@ def prepare_postgres():
             returncode=result.returncode,
         )
 
-    # yield
+    yield
 
-    # subprocess.check_output([TEARDOWN_PG_PATH], timeout=2 * 60)
+    subprocess.check_output([TEARDOWN_PG_PATH], timeout=2 * 60)
 
 
 @pytest.mark.parametrize('folder', list(get_test_folders()))
 def test_odyssey_functional(folder):
     runner = os.path.join(folder, RUNNER_SCRIPT_FILE)
-    result = subprocess.run(['/bin/bash', runner],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT,
-                            text=True)
-    if result.returncode != 0:
+    process = subprocess.run(['/bin/bash', runner])
+    retcode = process.returncode
+    if retcode != 0:
         pytest.fail(
-            f"Process {runner} exited with code {result.returncode}\n"
-            f"STDOUT:\n{result.stdout}\n"
-            f"STDERR:\n{result.stderr}\n"
+            f"Process {runner} exited with code {retcode}\n"
+            f"STDOUT:\n{process.stdout}\n"
+            f"STDERR:\n{process.stderr}\n"
         )
 
 
