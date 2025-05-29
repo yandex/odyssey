6c7f89c0e43c4f55d431bbea5596115a452238c4
reshke <reshke@double.cloud>
2025-05-29 18:11:44 +0500

    Update runner.sh

--- docker/functional/tests/virtual_processing/runner.sh
+++ docker/functional/tests/virtual_processing/runner.sh
@@ -4,12 +4,12 @@ set -ex
 
 # TODO: create more cool tests
 
-/usr/bin/odyssey /virtual_processing/odyssey.conf
+/usr/bin/odyssey /tests/virtual_processing/odyssey.conf
 sleep 1
 
-mkdir /virtual_processing/results/
-cat /virtual_processing/sql/virtual_show.sql | psql 'host=localhost port=6432 user=postgres dbname=postgres' --echo-all --quiet > /virtual_processing/results/virtual_show.out 2>&1
+mkdir /tests/virtual_processing/results/
+cat /tests/virtual_processing/sql/virtual_show.sql | psql 'host=localhost port=6432 user=postgres dbname=postgres' --echo-all --quiet > /tests/virtual_processing/results/virtual_show.out 2>&1
 
-diff /virtual_processing/expected/virtual_show.out /virtual_processing/results/virtual_show.out
+diff /tests/virtual_processing/expected/virtual_show.out /tests/virtual_processing/results/virtual_show.out
 
 ody-stop
