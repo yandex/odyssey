d6b1941e042ea7c8625ae850ea742ccac96cca1b
rkhapov <r.khapov@ya.ru>
2025-05-31 16:33:06 +0000

    endpoints_reader -> od_parse_addresses
    And also new od_address_t type for future usage
    
    Signed-off-by: rkhapov <r.khapov@ya.ru>

--- test/odyssey/test_address_parse.c
+++ test/odyssey/test_address_parse.c
@@ -7,7 +7,7 @@ typedef struct {
 	int port;
 } parse_test_arg_t;
 
-static inline parse_test_arg_t endp(const char *h, const char *z, int p)
+static inline parse_test_arg_t address(const char *h, const char *z, int p)
 {
 	parse_test_arg_t r = { .host = h, .zone = z, .port = p };
 
@@ -17,10 +17,10 @@ static inline parse_test_arg_t endp(const char *h, const char *z, int p)
 static inline void do_test(const char *line, int expected_rc,
 			   size_t expected_count, ...)
 {
-	od_storage_endpoint_t *endpoints = NULL;
+	od_address_t *addresses = NULL;
 	size_t count = 0;
 
-	int rc = od_parse_endpoints(line, &endpoints, &count);
+	int rc = od_parse_addresses(line, &addresses, &count);
 	test(rc == expected_rc);
 	test(count == expected_count);
 
@@ -31,74 +31,78 @@ static inline void do_test(const char *line, int expected_rc,
 		parse_test_arg_t expected_endpoint =
 			va_arg(args, parse_test_arg_t);
 
-		test(strcmp(expected_endpoint.host, endpoints[i].host) == 0);
+		test(strcmp(expected_endpoint.host, addresses[i].host) == 0);
 		test(strcmp(expected_endpoint.zone,
-			    endpoints[i].availability_zone) == 0);
-		test(expected_endpoint.port == endpoints[i].port);
+			    addresses[i].availability_zone) == 0);
+		test(expected_endpoint.port == addresses[i].port);
 
-		free(endpoints[i].host);
+		free(addresses[i].host);
 	}
 
-	free(endpoints);
+	free(addresses);
 
 	va_end(args);
 }
 
-void odyssey_test_endpoints_parse(void)
+void odyssey_test_address_parse(void)
 {
-	do_test("localhost", OK_RESPONSE, 1, endp("localhost", "", 0));
+	do_test("localhost", OK_RESPONSE, 1, address("localhost", "", 0));
 
-	do_test("[localhost]", OK_RESPONSE, 1, endp("localhost", "", 0));
+	do_test("[localhost]", OK_RESPONSE, 1, address("localhost", "", 0));
 
 	do_test("sas-sokkge3ejever6ae.mdb.yandex.net", OK_RESPONSE, 1,
-		endp("sas-sokkge3ejever6ae.mdb.yandex.net", "", 0));
+		address("sas-sokkge3ejever6ae.mdb.yandex.net", "", 0));
 
 	do_test("sas-sokkge3ejever6ae.mdb.yandex.net:1337", OK_RESPONSE, 1,
-		endp("sas-sokkge3ejever6ae.mdb.yandex.net", "", 1337));
+		address("sas-sokkge3ejever6ae.mdb.yandex.net", "", 1337));
 
 	do_test("sas-sokkge3ejever6ae.mdb.yandex.net:1337:sas", OK_RESPONSE, 1,
-		endp("sas-sokkge3ejever6ae.mdb.yandex.net", "sas", 1337));
+		address("sas-sokkge3ejever6ae.mdb.yandex.net", "sas", 1337));
 
 	do_test("sas-sokkge3ejever6ae.mdb.yandex.net:sas", OK_RESPONSE, 1,
-		endp("sas-sokkge3ejever6ae.mdb.yandex.net", "sas", 0));
+		address("sas-sokkge3ejever6ae.mdb.yandex.net", "sas", 0));
 
 	do_test("sas-sokkge3ejever6ae.mdb.yandex.net:sas:klg", NOT_OK_RESPONSE,
 		0);
 
-	do_test("sas-sokkge3ejever6ae.mdb.yandex.net:sas:1337", OK_RESPONSE, 1,
-		endp("sas-sokkge3ejever6ae.mdb.yandex.net", "sas", 1337));
+	do_test("sas-sokkge3ejever6ae.mdb.yandex.net:sas, sas-sokkge3ejever6ae.mdb.yandex.net:sas:klg",
+		NOT_OK_RESPONSE, 0);
 
-	do_test("[::1]:sas:1337", OK_RESPONSE, 1, endp("::1", "sas", 1337));
+	do_test("sas-sokkge3ejever6ae.mdb.yandex.net:sas:1337", OK_RESPONSE, 1,
+		address("sas-sokkge3ejever6ae.mdb.yandex.net", "sas", 1337));
+
+	do_test("[::1]:sas:1337", OK_RESPONSE, 1, address("::1", "sas", 1337));
 
 	do_test("sas-sokkge3ejever6ae.mdb.yandex.net,localhost,klg-kd19rbngltphuob9.mdb.yandex.net",
 		OK_RESPONSE, 3,
-		endp("sas-sokkge3ejever6ae.mdb.yandex.net", "", 0),
-		endp("localhost", "", 0),
-		endp("klg-kd19rbngltphuob9.mdb.yandex.net", "", 0));
+		address("sas-sokkge3ejever6ae.mdb.yandex.net", "", 0),
+		address("localhost", "", 0),
+		address("klg-kd19rbngltphuob9.mdb.yandex.net", "", 0));
 
 	do_test("sas-sokkge3ejever6ae.mdb.yandex.net:1337,localhost,klg-kd19rbngltphuob9.mdb.yandex.net:31337",
 		OK_RESPONSE, 3,
-		endp("sas-sokkge3ejever6ae.mdb.yandex.net", "", 1337),
-		endp("localhost", "", 0),
-		endp("klg-kd19rbngltphuob9.mdb.yandex.net", "", 31337));
+		address("sas-sokkge3ejever6ae.mdb.yandex.net", "", 1337),
+		address("localhost", "", 0),
+		address("klg-kd19rbngltphuob9.mdb.yandex.net", "", 31337));
 
 	do_test("sas-sokkge3ejever6ae.mdb.yandex.net:1337,[localhost],klg-kd19rbngltphuob9.mdb.yandex.net:31337",
 		OK_RESPONSE, 3,
-		endp("sas-sokkge3ejever6ae.mdb.yandex.net", "", 1337),
-		endp("localhost", "", 0),
-		endp("klg-kd19rbngltphuob9.mdb.yandex.net", "", 31337));
+		address("sas-sokkge3ejever6ae.mdb.yandex.net", "", 1337),
+		address("localhost", "", 0),
+		address("klg-kd19rbngltphuob9.mdb.yandex.net", "", 31337));
 
 	do_test("sas-sokkge3ejever6ae.mdb.yandex.net:1337:sas,localhost:5432:vla,klg-kd19rbngltphuob9.mdb.yandex.net:31337:klg",
 		OK_RESPONSE, 3,
-		endp("sas-sokkge3ejever6ae.mdb.yandex.net", "sas", 1337),
-		endp("localhost", "vla", 5432),
-		endp("klg-kd19rbngltphuob9.mdb.yandex.net", "klg", 31337));
+		address("sas-sokkge3ejever6ae.mdb.yandex.net", "sas", 1337),
+		address("localhost", "vla", 5432),
+		address("klg-kd19rbngltphuob9.mdb.yandex.net", "klg", 31337));
 
 	do_test("[2001:0db8:85a3:0000:0000:8a2e:0370:7334]:1337:sas,localhost:5432:vla,[2001:0db8:85a3:1234:5678:8a2e:1375:7334]:31337:klg",
 		OK_RESPONSE, 3,
-		endp("2001:0db8:85a3:0000:0000:8a2e:0370:7334", "sas", 1337),
-		endp("localhost", "vla", 5432),
-		endp("2001:0db8:85a3:1234:5678:8a2e:1375:7334", "klg", 31337));
+		address("2001:0db8:85a3:0000:0000:8a2e:0370:7334", "sas", 1337),
+		address("localhost", "vla", 5432),
+		address("2001:0db8:85a3:1234:5678:8a2e:1375:7334", "klg",
+			31337));
 
 	do_test("2001:0db8:85a3:0000:0000:8a2e:0370:7334:1337:sas,localhost:5432:vla,[2001:0db8:85a3:1234:5678:8a2e:1375:7334]:31337:klg",
 		NOT_OK_RESPONSE, 0);
