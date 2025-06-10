16efe04c57d568042b6245da9c2800aef9cbe7f7
heifetz <sergey.kheyfets@yandex.ru>
2025-06-10 22:46:00 +0500

    fix formatting

--- third_party/machinarium/sources/mutex.c
+++ third_party/machinarium/sources/mutex.c
@@ -72,7 +72,8 @@ static inline int mm_mutex_try_lock_fast(mm_mutex_t *mutex)
 	}
 
 	for (int i = 0; i < MAX_SPIN_COUNT; ++i) {
-		if (atomic_exchange(&mutex->state, MM_MUTEX_LOCKED) == MM_MUTEX_UNLOCKED) {
+		if (atomic_exchange(&mutex->state, MM_MUTEX_LOCKED) ==
+		    MM_MUTEX_UNLOCKED) {
 			mm_mutex_owner_t owner;
 			init_owner(&owner);
 
@@ -113,7 +114,8 @@ static inline int mm_mutex_lock_slow_attempt(mm_mutex_t *mutex,
 	mm_sleeplock_unlock(&mutex->queue_lock);
 
 	/* someone released lock, lets try to acquire */
-	if (atomic_exchange(&mutex->state, MM_MUTEX_LOCKED) == MM_MUTEX_UNLOCKED) {
+	if (atomic_exchange(&mutex->state, MM_MUTEX_LOCKED) ==
+	    MM_MUTEX_UNLOCKED) {
 		return 1;
 	}
 
