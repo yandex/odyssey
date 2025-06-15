73524afbd7b810bc34e31cdc684c20c8c891f16e
rkhapov <r.khapov@ya.ru>
2025-06-15 07:35:32 +0000

    docker/stress: more variant stress scenarios
    Signed-off-by: rkhapov <r.khapov@ya.ru>

--- docker/stress/primary-init/03-create-users.sql
+++ docker/stress/primary-init/03-create-users.sql
@@ -6,5 +6,8 @@ CREATE USER tuser WITH PASSWORD 'postgres';
 GRANT ALL ON DATABASE postgres TO tuser;
 GRANT ALL ON SCHEMA public TO tuser;
 
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT,INSERT,UPDATE,DELETE ON TABLES TO suser, tuser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE,SELECT ON SEQUENCES TO suser, tuser;
+
 CREATE ROLE replicator WITH REPLICATION LOGIN PASSWORD 'postgres';
 SELECT pg_create_physical_replication_slot('replication_slot');
