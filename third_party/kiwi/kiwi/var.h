9d1e7ffdc2301abb37597a513fc1a1962dc03724
reshke <reshke@double.cloud>
2025-07-31 06:00:29 +0000

    format

--- third_party/kiwi/kiwi/var.h
+++ third_party/kiwi/kiwi/var.h
@@ -162,8 +162,7 @@ static inline void kiwi_vars_init(kiwi_vars_t *vars)
 	/* XXX: todo - also accept aliases */
 	kiwi_var_init(&vars->vars[KIWI_VAR_ODYSSEY_TARGET_SESSION_ATTRS],
 		      "target_session_attrs", sizeof("target_session_attrs"));
-	kiwi_var_init(&vars->vars[KIWI_VAR_ROLE], "role",
-		      sizeof("role"));
+	kiwi_var_init(&vars->vars[KIWI_VAR_ROLE], "role", sizeof("role"));
 }
 
 static inline int kiwi_vars_set(kiwi_vars_t *vars, kiwi_var_type_t type,
