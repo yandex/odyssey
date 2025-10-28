16c37269dc85f4672efdea9465ba48e638769a90
roman khapov <r.khapov@ya.ru>
2025-10-28 05:42:00 +0000

    follow-up: check for long appnames
    Signed-off-by: roman khapov <r.khapov@ya.ru>

--- docker/functional/tests/query_processing/sql/application_name.sql
+++ docker/functional/tests/query_processing/sql/application_name.sql
@@ -2,3 +2,6 @@ SELECT 'z';
 
 SET application_name to 'AAA';
 SHOW application_name;
+
+SET application_name to 'This-is-long-application-name-that-must-be-cut-to-add-host-and-this-is-tail';
+SHOW application_name;
