98ec1fd5a824b2bf6ed8122207a6e2fcf17c09fe
Roman Khapov <r.khapov@yandex.ru>
2025-10-29 13:50:27 +0500

    fix test for prom exporter (#1030)
    Follow-up: eb66cfb
    
    Signed-off-by: roman khapov <r.khapov@ya.ru>

--- docker/prom-exporter/entrypoint.sh
+++ docker/prom-exporter/entrypoint.sh
@@ -32,4 +32,12 @@ sleep 1
 
 curl tester:9876/metrics -s | grep 'odyssey_' | grep -v '#' | grep -v 'odyssey_exporter_build_info' | grep -v 'odyssey_version_info' | grep -v 'odyssey_pool_postgres_postgres_bytes_received' | grep -v 'odyssey_pool_postgres_postgres_bytes_sent' > result.out
 
+echo EXPECTED START
+cat expected.out
+echo EXPECTED END
+echo
+echo RESULT START
+cat result.out
+echo RESULT END
+
 diff expected.out result.out
