6fd977aa5151513eaf2c479d166f6c5c9776ec7f
roman khapov <r.khapov@ya.ru>
2026-01-18 07:06:37 +0000

    disable pool_discard if use reserve_prepared_statement
    They conflicts
    
    Signed-off-by: roman khapov <r.khapov@ya.ru>

--- sources/rules.c
+++ sources/rules.c
@@ -1673,19 +1673,19 @@ int od_pool_validate(od_logger_t *logger, od_rule_pool_t *pool, char *db_name,
 	/* reserve prepare statement feature */
 	if (pool->reserve_prepared_statement &&
 	    pool->pool_type == OD_RULE_POOL_SESSION) {
-		/*
-		 * silently disable reserve_prepared_statement
-		 * for session pooling
-		 */
 		pool->reserve_prepared_statement = 0;
+
+		od_glog("rules", NULL, NULL,
+			"rule '%s.%s %s %s': disable prepared statements reserving due to session pooling",
+			db_name, user_name, address_range->string_value);
 	}
 
 	if (pool->reserve_prepared_statement && pool->discard) {
-		od_error(
-			logger, "rules", NULL, NULL,
-			"rule '%s.%s %s': pool discard is forbidden when using prepared statements support",
+		pool->discard = 0;
+
+		od_glog("rules", NULL, NULL,
+			"rule '%s.%s %s %s': disable pool discard due to prepared statements reserving",
 			db_name, user_name, address_range->string_value);
-		return NOT_OK_RESPONSE;
 	}
 
 	if (pool->smart_discard && !pool->reserve_prepared_statement) {
