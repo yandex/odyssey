7f816933b48bacacfe4f0b083ea415b45e31788a
roman khapov <r.khapov@ya.ru>
2026-04-10 10:26:41 +0000

    fix
    Signed-off-by: roman khapov <r.khapov@ya.ru>

--- test/functional/tests/idle_timeout/sql/session.sql
+++ test/functional/tests/idle_timeout/sql/session.sql
@@ -1,6 +1,9 @@
 -- no connection acquired
 \! sleep 3
 
+-- long active query works
+select pg_sleep(6);
+
 select 43;
 
 \! sleep 6
--- test/functional/tests/idle_timeout/sql/tx.sql
+++ test/functional/tests/idle_timeout/sql/tx.sql
@@ -5,6 +5,9 @@ select 53;
 \! sleep 1
 select 73;
 
+-- long op is not broken
+select pg_sleep(6);
+
 \! sleep 6
 
 select 76;
