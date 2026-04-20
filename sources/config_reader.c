39557a7ad94988fb83c107028cf617e83d4bf3aa
Evgeny Kuzin <evgeny.kuzin@outlook.com>
2026-04-20 17:29:13 -0400

    client_max 0 according to the doc

--- sources/config_reader.c
+++ sources/config_reader.c
@@ -1973,7 +1973,9 @@ static int od_config_reader_rule_settings(od_config_reader_t *reader,
 						     &rule->client_max)) {
 				return NOT_OK_RESPONSE;
 			}
-			rule->client_max_set = 1;
+			if (config->client_max > 0) {
+                config->client_max_set = 1;
+            }
 			continue;
 		/* client_fwd_error */
 		case OD_LCLIENT_FWD_ERROR:
@@ -3353,7 +3355,9 @@ static int od_config_reader_parse(od_config_reader_t *reader,
 						     &config->client_max)) {
 				goto error;
 			}
-			config->client_max_set = 1;
+			if (config->client_max > 0) {
+                config->client_max_set = 1;
+            }
 			continue;
 		/* client_max_routing */
 		case OD_LCLIENT_MAX_ROUTING:
