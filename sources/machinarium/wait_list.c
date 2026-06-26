fd2eaaceca514e7347cf4cbefcac4bfcca3392d8
Kirill Bratanov <74261401+Skiftare@users.noreply.github.com>
2026-06-26 10:48:23 +0500

    fixed deadcode at releasing sleepy

--- sources/machinarium/wait_list.c
+++ sources/machinarium/wait_list.c
@@ -264,7 +264,7 @@ int mm_wait_list_waitv(mm_wait_list_t **wait_lists, size_t count,
 	/* wait for any listener to signal shared_event */
 	(void)mm_eventmgr_wait(&mm_self->event_mgr, &shared_event, timeout_ms);
 
-	for (size_t i = 0; i > 0; --i) {
+	for (size_t i = count; i > 0; --i) {
 		release_sleepy_with_lock(wait_lists[i], &sleepy_list[i]);
 	}
 	mm_free(sleepy_list);
