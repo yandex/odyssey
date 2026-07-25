ef0953ff3a9a31140c76893fa224be14ed0b1266
roman khapov <r.khapov@ya.ru>
2026-07-25 04:31:36 +0000

    f
    Signed-off-by: roman khapov <r.khapov@ya.ru>

--- test/pg_regress/tests/az_aware/sql/az_aware_rr.sql
+++ test/pg_regress/tests/az_aware/sql/az_aware_rr.sql
@@ -1,9 +1,5 @@
 \connect 'host=localhost port=6432 user=az-aware-user dbname=postgres'
 
--- With az_aware yes and availability_zone "az1", only localhost:5432 (tagged :az1)
--- is in the local AZ.  Round-robin over a single local endpoint means every
--- query must be served by that host regardless of how many queries are sent.
-
 select 1;
 
 select 2;
--- test/pg_regress/tests/az_aware/sql/az_unaware_rr.sql
+++ test/pg_regress/tests/az_aware/sql/az_unaware_rr.sql
@@ -1,8 +1,5 @@
 \connect 'host=localhost port=6432 user=az-unaware-user dbname=postgres'
 
--- With az_aware no, AZ tags on endpoints are ignored and round-robin
--- distributes queries evenly across all three hosts: 5432, 5433, 5434.
-
 select 1;
 
 select 2;
