60679bc8709d902a925b86dd22f57cc2abafdd9d
Roman Khapov <r.khapov@yandex.ru>
2026-07-31 18:56:51 +0500

    fix scram auth with plain password on both sides (#1493)
    Leftover from e6cc6be
    
    Signed-off-by: roman khapov <r.khapov@ya.ru>

--- test/functional/tests/scram/test_scram_backend.sh
+++ test/functional/tests/scram/test_scram_backend.sh
@@ -30,3 +30,34 @@ for _ in $(seq 1 5); do
                 exit 1
         }
 done
+
+for _ in $(seq 1 5); do
+        PGPASSWORD=scram_user_password psql 'host=ip4-localhost port=6432 user=frontend_and_backend_auth_scram_password dbname=scram_db sslmode=disable' -c "SELECT 1" 2>&1 || {
+                echo "ERROR: failed frontend+backend auth with scram password"
+
+                cat /var/log/odyssey.log
+                echo "
+
+
+                "
+                cat /var/log/postgresql/postgresql-16-main.log
+
+                exit 1
+        }
+done
+
+for _ in $(seq 1 5); do
+        PGPASSWORD=scram_user_password_incorrect psql 'host=ip4-localhost port=6432 user=frontend_and_backend_auth_scram_password dbname=scram_db sslmode=disable' -c "SELECT 1" 2>&1 && {
+                echo "ERROR: successfully backend auth with incorrect password"
+
+                sleep 1
+                cat /var/log/odyssey.log
+                echo "
+
+
+                "
+                cat /var/log/postgresql/postgresql-16-main.log
+
+                exit 1
+        }
+done
