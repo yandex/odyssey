2610b822f991a82384990b333c1bf3a9e8b697f1
Roman Khapov <r.khapov@yandex.ru>
2026-08-27 07:35:12 +0500

    print asan output for prep_stmts test (#1544)
    Signed-off-by: roman khapov <r.khapov@ya.ru>

--- test/functional/tests/prep_stmts/runner.sh
+++ test/functional/tests/prep_stmts/runner.sh
@@ -5,6 +5,12 @@ set -ex
 /usr/bin/odyssey /tests/prep_stmts/pstmts.conf
 sleep 1
 
-/tests/prep_stmts/pstmts-test
+/tests/prep_stmts/pstmts-test || {
+    sleep 1
+    for i in /asan-output*; do
+        cat $i || true
+    done
+    exit 1
+}
 
 ody-stop
