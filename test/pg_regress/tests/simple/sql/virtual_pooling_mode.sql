444b4581b7a9ff26f5bd14a6964acf0307034936
reshke <reshke@double.cloud>
2026-09-17 20:36:01 +0500

    Add virtual "odyssey.pooling_mode" GUC. (#1607)
    With virtual processing enabled, client can inspect
    its current connection pool mode state. This GUC is however
    unchangable for session. One should use session pin feature instread.
    
    This  also exposed a notice bug:
    kiwi_be_write_notice wrote "NOTICE\0s" after the 'S' field
    and psql printed junk:
    
    psql:/tmp/t.sql:2: NOTICE:  processed virtually by odyssey
    SCHEMA NAME:
    psql:/tmp/t.sql:2: ERROR:  55P02: parameter "odyssey.pooling_mode" cannot be changed now

---
+++ test/pg_regress/tests/simple/sql/virtual_pooling_mode.sql
@@ -0,0 +1,20 @@
+
+\set VERBOSITY verbose
+
+\c postgres suser
+SHOW odyssey.pooling_mode;
+
+SET odyssey.pooling_mode = 'transaction';
+SHOW odyssey.pooling_mode;
+
+\c postgres tuser
+SHOW odyssey.pooling_mode;
+
+SET odyssey.pooling_mode = 'session';
+SHOW odyssey.pooling_mode;
+
+\c postgres stuser
+SHOW odyssey.pooling_mode;
+
+\c postgres suser
+SHOW odyssey.pooling_mode;
